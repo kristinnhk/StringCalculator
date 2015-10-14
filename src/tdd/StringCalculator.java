b20af8a6d1cfd621b58a1980ab68eef73a58ce4f
kristinnhk <kristinnhk@gmail.com>
2015-10-14 17:59:41 +0000

    step 1 finished

---
+++ src/tdd/StringCalculator.java
@@ -0,0 +1,30 @@
+package tdd;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class StringCalculator {
+	public static void main(String args[]){
+		StringCalculator calc = new StringCalculator();
+		
+		In in = new In();
+		String numbers = in.readLine();
+		calc.Add(numbers);
+	}
+	
+	public int Add(String numbers){
+		if(numbers.isEmpty()) return 0;
+		int result = 0;
+		Pattern p = Pattern.compile("-?\\d+");
+		Matcher m = p.matcher(numbers);
+		while (m.find()) {
+			result += Integer.parseInt(m.group());
+			System.out.println(m.group());
+		}
+		System.out.println(result);
+		return 0;
+	}
+}
