3ac518dfb4dddc956c89a906d728b8cd81806e18
kristinnhk <kristinnhk@gmail.com>
2015-10-14 22:10:34 +0000

    step 5 complete

--- src/tdd/StringCalculator.java
+++ src/tdd/StringCalculator.java
@@ -1,5 +1,7 @@
 package tdd;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,12 +11,12 @@ import edu.princeton.cs.algs4.StdOut;
 
 
 //test 1. complete
+//realtest 2 didn't have to change anything either
+//test 2. didn't have to change anything(was actually test nr 3 )
 
-//test 2. didn't have to change anything
-
-//test 3 my method doesn't specifically read delimiters but plucks the numbers
+//test 4 my method doesn't specifically read delimiters but plucks the numbers
 //out of a string, it works on all test cases and i hope it's fine
-//although it doesn't use delimiters like i said.
+//although it doesn't use delimiters like i said. (
 
 public class StringCalculator {
 	public static void main(String args[]){
@@ -28,13 +30,35 @@ public class StringCalculator {
 	public int Add(String numbers){
 		if(numbers.isEmpty()) return 0;
 		int result = 0;
+		List<Integer> negative = new ArrayList<Integer>();
 		Pattern p = Pattern.compile("-?\\d+");
 		Matcher m = p.matcher(numbers);
-		while (m.find()) {
-			result += Integer.parseInt(m.group());
-			System.out.println(m.group()); //just for my own purpose while programming
+		try {
+			while (m.find()) {
+				int temp = Integer.parseInt(m.group());
+				if(temp > 0){
+					result += temp;
+					//System.out.println(m.group()); //just for my own purpose while programming
+				}
+				else{
+					negative.add(temp);
+				}
+			
+			}
+			if(negative.size() > 0) throw new Exception();
+			System.out.println(result);
+			
+		} catch (Exception e) {
+			System.out.print("\"Negatives not allowed: ");
+			for(int i = 0; i < negative.size(); i++){
+				
+				System.out.print(negative.get(0));
+				if(i+1 == negative.size()) System.out.println("\"");
+				else System.out.print(",");
+			}
+			
 		}
-		System.out.println(result);
+		
 		return 0;
 	}
 }
